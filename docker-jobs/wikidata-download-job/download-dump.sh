97de62e28501ee45370c0271ed7a88e787091baf
thewillyhuman <colunga91@gmail.com>
2021-06-03 21:53:06 +0200

    Split ttl and json dump jobs

--- docker-jobs/wikidata-download-job/download-dump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = 'ttl' ]; then
-    wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.ttl.bz2
-else
-    wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2
-fi
-
-exec "$@"
