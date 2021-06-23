320c8bb42a067c3436c58356ec63c8fb511ee8d3
Fidalgo <UO258524@uniovi.es>
2021-06-23 12:59:32 +0200

    Test method to measure paginated, parametrized queries

--- query-timer/src/main/java/es/weso/Main.java
+++ query-timer/src/main/java/es/weso/Main.java
@@ -10,26 +10,39 @@ import org.eclipse.rdf4j.query.QueryEvaluationException;
 
 public class Main {
 
+    private final static String BLAZEGRAPH_ENDPOINT = "http://localhost:9999/blazegraph/sparql";
+
     private final static String WIKIDATA_ENDPOINT = "https://query.wikidata.org/sparql";
 
-    private final static int REPETITIONS = 10;
+    private final static int REPETITIONS = 1;
 
     private static FileWriter fw;
 
     private final static List<String> queries = new ArrayList<String>(Arrays.asList(
-    "/query-timer/src/main/resources/luxembourg_authors.sparql",
-    "/query-timer/src/main/resources/spain_authors.sparql",
-    "/query-timer/src/main/resources/university_oxford_topics.sparql",
-    "/query-timer/src/main/resources/university_oviedo_topics.sparql"
+    "/query-timer/src/main/resources/country_authors.sparql"
+    //"/query-timer/src/main/resources/luxembourg_authors.sparql",
+    //"/query-timer/src/main/resources/spain_authors.sparql",
+    //"/query-timer/src/main/resources/university_oxford_topics.sparql",
+    //"/query-timer/src/main/resources/university_oviedo_topics.sparql"
+    //"/query-timer/src/main/resources/luxembourg_authors_blns.sparql"
+    //"/query-timer/src/main/resources/luxembourg_authors_wdsq.sparql"
+    ));
+
+    private final static List<String> parameters = new ArrayList<String>(Arrays.asList(
+    "Q778",
+    "Q32",
+    "Q29",
+    "Q183",
+    "Q30"
     ));
 
     public static void main(String[] args){
         try {
-            fw = new FileWriter("./query-timer/responsetimes.csv");
-            fw.write("Query,Mean,Standard Deviation,Longest Time,Shortest Time,Number of nodes (x̄),Number of nodes (SD)\n");
+            fw = new FileWriter("./query-timer/responsetimes_bl.csv");
             for (String query : queries) {
                 try {       
-                    measureQuery(query);         
+                    //measureQuery(query);
+                    measurePaginatedQuery(query);      
                 } catch (IOException e) {
                     System.out.println("An error has taken place while reading query " + query 
                     + " or writing its response times.");
@@ -52,7 +65,7 @@ public class Main {
     }
 
     private static void measureQuery(String query) throws IOException {
-
+        fw.write("Query,Mean,Standard Deviation,Longest Time,Shortest Time,Number of nodes (x̄),Number of nodes (SD)\n");
         SPARQLTimer timer = new SPARQLTimer();
         System.out.println("\n>>>>>>>>>>><<<<<<<<<<");
         String queryName = query.split("resources/")[1].replace(".sparql", "").toUpperCase();
@@ -67,7 +80,7 @@ public class Main {
 
         for(int i = 0; i < REPETITIONS; i++) {
             String data = SPARQLReader.readFile(query);
-            timer.executeQuery(data, WIKIDATA_ENDPOINT);
+            timer.executeQuery(data, BLAZEGRAPH_ENDPOINT);
             double responseTime = timer.getExecutionTime();
             System.out.println("Iteration " + i + ": " + responseTime);
             total += responseTime;
@@ -92,6 +105,60 @@ public class Main {
         System.out.println("NUMBER OF NODES (SD): " + sdNodes);
         fw.write(String.format("%s,%d,%d,%d,%d,%d,%d\n", queryName, (int) mean, (int) sd, (int) longer, (int) shorter,
             (int) meanNodes, (int) sdNodes));
+    }
+
+    private static void measurePaginatedQuery(String query) throws IOException {
+        fw.write("Query,Mean,Standard Deviation,Longest Time,Shortest Time,Number of nodes (x̄),Number of nodes (SD),Number of queries (x̄),Number of queries (SD)\n");
+        SPARQLTimer timer = new SPARQLTimer();
+        System.out.println("\n>>>>>>>>>>><<<<<<<<<<");
+        String queryName = query.split("resources/")[1].replace(".sparql", "").toUpperCase();
+        System.out.println("Measuring query " + queryName);
+
+        double total = 0;
+        double[] values = new double[REPETITIONS];
+        double[] nodeValues = new double[REPETITIONS];
+        double[] queryValues = new double[REPETITIONS];
+        double longer = -1;
+        double shorter = Double.MAX_VALUE;
+        int totalnodes = 0;
+
+        int totalQueries = 0;
+
+        for(int i = 0; i < REPETITIONS; i++) {
+            String data = SPARQLReader.readFile(query);
+            timer.executePaginatedQuery(parameters, data, WIKIDATA_ENDPOINT);
+            double responseTime = timer.getTotalTime();
+            System.out.println("Iteration " + i + ": " + responseTime);
+            total += responseTime;
+            values[i] = responseTime;
+            longer = MathUtil.getLarger(longer, responseTime);
+            shorter = MathUtil.getShorter(shorter, responseTime);
+            int numberOfNodes = timer.getNumberOfNodes();
+            totalnodes += numberOfNodes;
+            nodeValues[i] = numberOfNodes;
+            int numberOfQueries = timer.getTotalQueries();
+            totalQueries += numberOfQueries;
+            queryValues[i] = numberOfQueries;
+        }   
+        double mean = MathUtil.getMean(total, REPETITIONS);
+        double sd = MathUtil.calculateSD(values, mean);
+
+        double meanNodes = MathUtil.getMean(totalnodes, REPETITIONS);
+        double sdNodes = MathUtil.calculateSD(nodeValues, meanNodes);
+
+        double meanQueries = MathUtil.getMean(totalQueries, REPETITIONS);
+        double sdQueries = MathUtil.calculateSD(queryValues, meanQueries);
+
+        System.out.println("MEAN: " + mean);
+        System.out.println("STANDARD DEVIATION: " + sd);
+        System.out.println("LONGEST RESPONSE TIME: " + longer);
+        System.out.println("SHORTEST RESPONSE TIME: " + shorter);
+        System.out.println("NUMBER OF NODES (x̄): " + meanNodes);
+        System.out.println("NUMBER OF NODES (SD): " + sdNodes);
+        System.out.println("NUMBER OF QUERIES (x̄): " + meanQueries);
+        System.out.println("NUMBER OF QUERIES (SD): " + sdQueries);
+        fw.write(String.format("%s,%d,%d,%d,%d,%d,%d,%d,%d\n", queryName, (int) mean, (int) sd, (int) longer, (int) shorter,
+            (int) meanNodes, (int) sdNodes,  (int) meanQueries, (int) sdQueries));
     }
 
     
--- query-timer/src/main/java/es/weso/SPARQLTimer.java
+++ query-timer/src/main/java/es/weso/SPARQLTimer.java
@@ -14,23 +14,84 @@ public class SPARQLTimer {
 
     private double initTime;
     private double finalTime;
+    private double totalTime;
+    private int totalQueries;
     private int numberOfNodes;
 
     public void executeQuery(String query,String endpoint) throws QueryEvaluationException {
 
-        startCrono();
-
         SPARQLRepository sparqlRepository = new SPARQLRepository(endpoint);
         RepositoryConnection sparqlConnection = sparqlRepository.getConnection();
+
+        startCrono();
 
         TupleQuery tupleQuery = sparqlConnection.prepareTupleQuery(
                 QueryLanguage.SPARQL, query);
 
         List<BindingSet> bindingSets = QueryResults.asList(tupleQuery.evaluate());
+        
+        stopCrono();
+
         numberOfNodes = bindingSets.size();
         System.out.println("NODES: " + numberOfNodes);
+    }
+
+    public void executePaginatedQuery(List<String> parameters, 
+        String query,String endpoint) throws QueryEvaluationException {
+
+        for (String pm : parameters) {
+            System.out.println("PARAMETER: " + pm);
+            totalTime = 0;
+            numberOfNodes = 0;
+            totalQueries = 0;
+            String parametrizedQuery = query.replace("{{ q }}", pm + "");
+            sequenceQueriesWithOffset(parametrizedQuery, 0, endpoint);
+            
+            System.out.println(totalTime + " s");
+            System.out.println(numberOfNodes  + " nodos");
+            System.out.println(totalQueries + " queries");
+        }
+
+        
+    }
+
+    private void sequenceQueriesWithOffset(String query, int offset, String endpoint) {
+
+        int i = offset;
+
+        try {
+            int dataSize = queryWithOffset(query, i, endpoint);
+            if(dataSize > 0) {
+                i += 1000;
+                sequenceQueriesWithOffset(query, i, endpoint);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+            sequenceQueriesWithOffset(query, i, endpoint);
+        }
+    }
+
+    private int queryWithOffset(String query, int offset, String endpoint) {
+
+        SPARQLRepository sparqlRepository = new SPARQLRepository(endpoint);
+        RepositoryConnection sparqlConnection = sparqlRepository.getConnection();
+
+        String offQuery = query.replace("{w}", offset + "");
+        
+        startCrono();
+ 
+        TupleQuery tupleQuery = sparqlConnection.prepareTupleQuery(
+                QueryLanguage.SPARQL, offQuery);
+
+        List<BindingSet> bindingSets = QueryResults.asList(tupleQuery.evaluate());
 
         stopCrono();
+
+        totalTime += getExecutionTime();
+        numberOfNodes += bindingSets.size();
+        totalQueries++;
+
+        return bindingSets.size();
     }
 
 
@@ -50,4 +111,11 @@ public class SPARQLTimer {
         return numberOfNodes;
     }
 
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public int getTotalQueries() {
+        return totalQueries;
+    }
 }
