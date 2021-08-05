222f440f40a7913eb531d06f6477916ccd8d635d
Fidalgo <UO258524@uniovi.es>
2021-08-05 11:07:30 +0200

    GridFS reading capabilities
    And Asia params to test

--- cacher/main.js
+++ cacher/main.js
@@ -10,7 +10,7 @@ async function executeQueries() {
     startTime = Date.now();
     for (const q of config) {
         await executePaginatedQuery(q.query, q.parameters, q.query_name, q.count_query)
-      }
+    }
     let t1 = Date.now();
     console.log("Synchronous time: " + (t1 - startTime) + " milliseconds.")
 }
@@ -20,56 +20,63 @@ async function executePaginatedQuery(query, parameters, qName, cQuery) {
         parametrizedQuery = query.replace(/{{ q }}/g, p);
         parametrizedCQuery = cQuery.replace(/{{ q }}/g, p);
         await sparqlLimits(parametrizedQuery, p, qName, parametrizedCQuery)
-      }
+    }
 }
 
 async function sparqlLimits(sparql, param, qName, cQuery) {
     qstartTime = Date.now();
-    var count_url = "https://query.wikidata.org/sparql?query=" + 
+    var count_url = "https://query.wikidata.org/sparql?query=" +
         encodeURIComponent(cQuery) + '&format=json';
 
     await fetch(count_url, {
-        method: 'GET',
-        headers: {
-            'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
-        }})
-    .then(res => res.json())
-    .then(async (json) => {
-        subsetting_size = json.results.bindings[0].subsetting_size.value;
-        console.log("Subsetting size: " + subsetting_size);
-    });
+            method: 'GET',
+            headers: {
+                'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
+            }
+        })
+        .then(res => res.json())
+        .then(async (json) => {
+            subsetting_size = json.results.bindings[0].subsetting_size.value;
+            console.log("Subsetting size: " + subsetting_size);
+        });
 
 
-    var url = "https://query.wikidata.org/sparql?query=" + 
+    var url = "https://query.wikidata.org/sparql?query=" +
         encodeURIComponent(sparql.replace(/\{w\}/, "0")) + '&format=json';
 
-    var extractedData = {"data": [], "parameter": param};
+    var extractedData = {
+        "data": [],
+        "parameter": param
+    };
     noResults = 0;
     console.log(param);
 
     await fetch(url, {
-        method: 'GET',
-        headers: {
-            'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
-        }})
-    .then(res => res.json())
-    .then(async (json) => {
+            method: 'GET',
+            headers: {
+                'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
+            }
+        })
+        .then(res => res.json())
+        .then(async (json) => {
 
-        extractedData.data = json;
+            extractedData.data = json;
 
-        await sequenceQueriesWithOffset(sparql, 1000, extractedData);
-        
-    })
-    .then(async () => {
-        await repositorio.conexion()
-            .then((db) => repositorio.removeResults(db, {"filename": qName + "_" + param}, qName));
-    })
-    .then(() => {
-        repositorio.conexion()
-            .then((gfs) => repositorio.insertResults(gfs, extractedData, qName, startTime));
-        console.log("Tiempo: " + (Date.now() - qstartTime))
-    });
-    
+            await sequenceQueriesWithOffset(sparql, 1000, extractedData);
+
+        })
+        .then(async () => {
+            await repositorio.conexion()
+                .then((db) => repositorio.removeResults(db, {
+                    "filename": qName + "_" + param
+                }, qName));
+        })
+        .then(() => {
+            repositorio.conexion()
+                .then((gfs) => repositorio.insertResults(gfs, extractedData, qName, startTime));
+            console.log("Tiempo: " + (Date.now() - qstartTime))
+        });
+
 }
 
 async function sequenceQueriesWithOffset(sparql, offset, extractedData) {
@@ -77,7 +84,7 @@ async function sequenceQueriesWithOffset(sparql, offset, extractedData) {
     await queryWithOffset(sparql, offset, extractedData).then(async function(quantity) {
         console.log("Nodes: " + extractedData.data.results.bindings.length);
         console.log("Offset " + offset)
-        if(offset < subsetting_size) {
+        if (offset < subsetting_size) {
             i += 1000;
             await sequenceQueriesWithOffset(sparql, i, extractedData);
         }
@@ -86,35 +93,35 @@ async function sequenceQueriesWithOffset(sparql, offset, extractedData) {
         //console.log(e);
         await sequenceQueriesWithOffset(sparql, i, extractedData);
     });
-    
+
 }
 
 async function queryWithOffset(sparql, offset, extractedData) {
-    let url = "https://query.wikidata.org/sparql?query=" + 
+    let url = "https://query.wikidata.org/sparql?query=" +
         encodeURIComponent(sparql.replace(/\{w\}/, offset.toString())) + '&format=json';
 
     return await fetch(url, {
-        method: 'GET',
-        headers: {
-            'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
-        }
-    })
-    .then((res) =>res.json())
-    .then((json) => {
-        var quantity = 0;
+            method: 'GET',
+            headers: {
+                'User-Agent': 'WesoScholiaCacher/0.1 (https://github.com/weso/weso-scholia)'
+            }
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            var quantity = 0;
 
-        json.results.bindings.forEach(function(row) {
-            //Comment condition if ordered subsetting
-            //if(!existsId(extractedData.data.results.bindings, row.hiddenId)) {
+            json.results.bindings.forEach(function(row) {
+                //Comment condition if ordered subsetting
+                //if(!existsId(extractedData.data.results.bindings, row.hiddenId)) {
                 extractedData.data.results.bindings.push(row);
-                quantity++;         
-            //}
+                quantity++;
+                //}
+            });
+
+            return quantity;
         });
-        
-        return quantity;
-    }); 
-    
-    
+
+
 }
 
 function existsId(collection, id) {
@@ -123,4 +130,10 @@ function existsId(collection, id) {
     });
 }
 
+function getFile(name) {
+    repositorio.conexion()
+        .then((gfs) => repositorio.getResult(gfs, name));
+}
+
 executeQueries();
+//getFile("country_authors_Q32")
--- cacher/repositorio.js
+++ cacher/repositorio.js
@@ -1,5 +1,5 @@
 module.exports = {
-    conexion : async () => {
+    conexion: async () => {
         let mongo = require("mongodb");
         let db = "mongodb://userb:JsE723U4sVoz1nGt@cluster0-shard-00-00.iygmf.mongodb.net:27017,cluster0-shard-00-01.iygmf.mongodb.net:27017,cluster0-shard-00-02.iygmf.mongodb.net:27017/scholia?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
         return new Promise((resolve, reject) => {
@@ -8,31 +8,33 @@ module.exports = {
                     console.log("Error when connecting to database")
                     resolve(null)
                 } else {
-                    var bucket = new mongo.GridFSBucket(db, {chunkSizeBytes: 16000000}); 
+                    var bucket = new mongo.GridFSBucket(db, {
+                        chunkSizeBytes: 16000000
+                    });
                     resolve(bucket);
                 }
             });
         });
     },
-    insertResults : async (db, results, col, t0) => {
+    insertResults: async (db, results, col, t0) => {
         return new Promise((resolve, reject) => {
             const fs = require('fs');
             let data = JSON.stringify(results);
             fs.writeFileSync('toUpload.json', data);
 
             fs.createReadStream("toUpload.json").
-                pipe(db.openUploadStream(col + "_" + results.parameter)).
-                on('error', function(error) {
-                    console.log(error)
-                    resolve(null);
-                }).
-                on('finish', function() {
-                    console.log("Saved successfully")
-                    console.log("Elapsed time: " + (Date.now() - t0) + " milliseconds.")
-                    db.s.db.close();
-                    resolve(true);
-                });
-              
+            pipe(db.openUploadStream(col + "_" + results.parameter)).
+            on('error', function(error) {
+                console.log(error)
+                resolve(null);
+            }).
+            on('finish', function() {
+                console.log("Saved successfully")
+                console.log("Elapsed time: " + (Date.now() - t0) + " milliseconds.")
+                db.s.db.close();
+                resolve(true);
+            });
+
             /* let collection = db.collection(col);
             collection.insert(results, (err, result) => {
                 if (err) {
@@ -45,29 +47,28 @@ module.exports = {
                 }
                 db.close();
             });*/
-        }); 
+        });
     },
-    removeResults : async (db, criterio, col) => {
+    removeResults: async (db, criterio, col) => {
         return new Promise((resolve, reject) => {
             db.find(criterio)
-            .toArray((err, files) => {
-            if (err) {
-                console.log("Error when removing results: " + err)
-                resolve(null);
-            }
-              if (!files || files.length === 0) {
-                console.log("No file to delete")
-                resolve(false)
-                db.s.db.close();
-              }
-              else {
-                db.delete(files[0]._id, function() {
-                    console.log('File Deleted')
-                    resolve(true)
-                    db.s.db.close();
-                });
-              }
-    })
+                .toArray((err, files) => {
+                    if (err) {
+                        console.log("Error when removing results: " + err)
+                        resolve(null);
+                    }
+                    if (!files || files.length === 0) {
+                        console.log("No file to delete")
+                        resolve(false)
+                        db.s.db.close();
+                    } else {
+                        db.delete(files[0]._id, function() {
+                            console.log('File Deleted')
+                            resolve(true)
+                            db.s.db.close();
+                        });
+                    }
+                })
             /* let collection = db.collection(col);
             collection.remove(criterio, (err, result) => {
                 if (err) {
@@ -78,6 +79,23 @@ module.exports = {
                 }
                 db.close();
             }); */
+        });
+    },
+    getResult: async (db, col) => {
+        return new Promise((resolve, reject) => {
+            const fs = require('fs');
+            db.openDownloadStreamByName(col).
+            pipe(fs.createWriteStream('./output.json')).
+            on('error', function(error) {
+                console.log(error);
+                resolve(null);
+                db.s.db.close();
+            }).
+            on('finish', function() {
+                console.log('done!');
+                resolve(true);
+                db.s.db.close();
+            });
         });
     },
 };
